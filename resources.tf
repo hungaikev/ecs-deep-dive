6bd4eb2001c39af52ebd4dc08b8964a7a8bfaa08
Hungai Amuhinda <hungaiinc@gmail.com>
2020-04-02 10:22:28 +0300

    Initial commit

---
+++ resources.tf
@@ -0,0 +1,27 @@
+# Prodiders
+
+provider "aws" {
+  profile = "default"
+  region = "eu-central-1"
+}
+
+# Data
+
+data "aws_availability_zones" "available" {}
+
+
+# Resources
+
+#Networking
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "Terraform"
+  cidr = "10.0.0.0/16"
+  azs = "${slice(data.aws_availability_zones.available.names,0 , var.subnet_count)}"
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
+  public_subnets = ["10.0.0.0/24", "10.0.2.0/24", "10.0.4.0/24"]
+  enable_nat_gateway = true
+  create_database_subnet_group = false
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+## Variables
+
+variable "subnet_count" {
+  default = 3
+}
